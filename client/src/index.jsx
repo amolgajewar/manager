078d02badba1a3df77f9a6e260d2bf5dfca3ee36
amol gajewar <amolgajewar@gmail.com>
2019-08-13 10:55:37 -0700

    renders App component to DOM

---
+++ client/src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App.jsx';
+//const projectArray = require('./dummyData.js');
+
+ReactDOM.render(<App />, document.getElementById('app'));
